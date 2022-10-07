61f885d7eeb8dab22128e7c6cef8a71cbb3c0688
kebench <kebenchshoppe@gmail.com>
2022-10-07 16:35:05 +0800

    Updates for the PHP code

--- aeropage.php
+++ aeropage.php
@@ -8,54 +8,70 @@
  * Author URI: https://tools.aeropage.io/
  * License: GPL2
 */
-
-
-
 add_action('admin_menu', 'aeropage_plugin_menu');
  
 function aeropage_plugin_menu(){
-add_menu_page( 'Aeropage Sync for Airtable', 'Aeropage', 'manage_options', 'aeropage' , 'aeropage_plugin_admin' );
+  add_menu_page( 'Aeropage Sync for Airtable', 'Aeropage', 'manage_options', 'aeropage' , 'aeroplugin_admin_page' );
 }
 
+/**
+ * Init Admin Page.
+ *
+ * @return void
+ */
+function aeroplugin_admin_page() {
+  require_once plugin_dir_path( __FILE__ ) . 'templates/app.php';
+  
+  //aeropageList();
+  
+}
+add_action( 'admin_enqueue_scripts', 'aeroplugin_admin_enqueue_scripts' );
 
-
-function aeropage_plugin_admin()
-{
-global $wpdb;
-
-$aero_posts = get_posts(['post_type' => 'aero-template','post_status' => 'private','numberposts' => -1]);
-
-//var_dump($aero_posts);
-
-
-$response = aeropageSyncPosts(71);
-
-echo $response['message'];
-
-
-//$aero_record_posts = get_posts(['post_type'=> 'aero-template','post_status' => 'private','numberposts' => 1,'meta_key' => 'aero_record']);
-
-
-//var_dump($aero_record_posts);
-
-
+/**
+ * Enqueue scripts and styles.
+ *
+ * @return void
+ */
+function aeroplugin_admin_enqueue_scripts() {
+  wp_enqueue_style( 'aeroplugin-style', plugin_dir_url( __FILE__ ) . 'build/index.css' );
+  wp_enqueue_script( 'aeroplugin-script', plugin_dir_url( __FILE__ ) . 'build/index.js', array( 'wp-element' ), date("h:i:s"), true );
+  wp_add_inline_script( 'aeroplugin-script', 'const MYSCRIPT = ' . json_encode( array(
+      'ajaxUrl' => admin_url( 'admin-ajax.php' ),
+      'plugin_admin_path' => parse_url(admin_url())["path"],
+      'plugin_name' => "aeropage" //This is the name of the plugin.
+  ) ), 'before' );
 }
 
-
-
+add_action("wp_ajax_aeropageList", "aeropageList");
 function aeropageList()
 {
 
-$aeroPosts = get_posts(['post_type' => 'aero-template','post_status' => 'private','numberposts' => -1]);
+  $aeroPosts = get_posts(['post_type' => 'aero-template','post_status' => 'private','numberposts' => -1]);
+	
+	foreach ($aeroPosts as $post)
+	{
+	$post->sync_status = get_post_meta($post->ID, "aero_sync_status",true);
+	$post->sync_time = get_post_meta($post->ID, "aero_sync_time",true);
+	$post->sync_message = get_post_meta($post->ID, "aero_sync_message",true);
+	}
+	
+  // this is for react...
 
-// this is for react...
-
-header('Content-Type: application/json');
-echo json_encode($aeroPosts);
-
+  header('Content-Type: application/json');
+  echo json_encode($aeroPosts);
+  die();
 }
 
 
+
+add_action( 'wp_ajax_get_token', 'aeropageEditorMeta');
+//Gets the aero page token when in the edit post
+function aeropageEditorMeta(){
+  $pid = $_POST["id"];
+  $token = get_post_meta($pid, "aero_token");
+  $status = get_post_meta($pid, "aero_sync_status");
+  die(json_encode(array("token" => $token,"status" => $status)));
+}
 
 
 // make sure all the custom post types are registered.
@@ -65,7 +81,7 @@ add_action( 'init', 'aeroRegisterTypes' );
 function aeroRegisterTypes()
 {
 
-$flush = null;
+    $flush = null;
 
 		$aeroPosts = get_posts(['post_type' => 'aero-template','post_status' => 'private','numberposts' => -1]);
 
@@ -106,151 +122,184 @@ $flush = null;
 		}
 
 		}
-
 		if ($flush){flush_rewrite_rules();}
 
 }
 
 
 
-
+add_action("wp_ajax_aeropageEdit", "aeropageEdit");
 function aeropageEdit() // called by ajax, adds the cpt
 {
-
-// can be passed an id (edit) or empty to create new.
-
+// can be passed an id (edit) or empty to create new
 // wordpress will automatically increment the slug if its already used.
 
-$template_post = array(
-        'ID' => $_POST['id'],
-        'post_title' => $_POST['title'],
-        'post_name' => $_POST['slug'],
-		'post_content'=> $_POST['token'],
-        'post_excerpt'=> $_POST['dynamic'],
-        'post_type' => 'aero-template',
-        'post_status' => 'private'
-    );
+  $template_post = array(
+          'ID' => $_POST['id'],
+          'post_title' => $_POST['title'],
+          'post_name' => $_POST['slug'],
+          'post_excerpt'=> $_POST['dynamic'],
+          'post_type' => 'aero-template',
+          'post_status' => 'private'
+      );
 
-$id = wp_insert_post($template_post);
+  $id = wp_insert_post($template_post);
 
-if ($id)
-{
-update_post_meta ($id,'aero_token',$_POST['token']);
-}
+  if ($id)
+  {
+  update_post_meta ($id,'aero_token',$_POST['token']);
+  aeropageSyncPosts($id);
+  }
 
-return $id;
+  die(json_encode(array("status" => "success", "post_id" => $id)));
 	
 }
-	
 
+// function aeropageTokenCheck()
+// {
+//   $token = $_POST["token"];
+//   aeropageTokenApiCall($token);
+// }
+
+add_action("wp_ajax_aeropageSyncPosts", "aeropageSyncPosts");
 
 function aeropageSyncPosts($parentId)
 {
 
-global $wpdb;
+  if(!$parentId)
+  {
+  $isAjax = true;
+  $parentId = $_POST["id"];
+  }
+ 
 
-$parent = get_post($parentId);
+  global $wpdb;
 
-$token = get_post_meta($parentId,'_aero_token',true);
+  $parent = get_post($parentId);
 
-$apiData = aeropageTokenApiCall($token);
+  $token = get_post_meta($parentId,'aero_token',true);
 
-$response = array();
+  $apiData = aeropageTokenApiCall($token);
 
-if ($apiData['status']['type'] == 'success' and $apiData['records'])
-{
+  $response = array();
 
+  if ($apiData['status']['type'] == 'success' and $apiData['records'])
+  {
+	$response['status'] = 'success';
+	update_post_meta ($parentId,'aero_sync_status','success');
+	update_post_meta ($parentId,'aero_sync_time',time());
+  
+  // trash posts 
 
-// trash posts 
+  $trash = "
+      UPDATE $wpdb->posts p
+          INNER JOIN $wpdb->postmeta pm ON (p.ID = pm.post_id AND pm.meta_key = '_aero_cpt') 
+      SET p.post_status = 'trash' 
+      WHERE pm.meta_value = '$parentId'"
+      ;
+  $results = $wpdb->get_results($trash);
+      
+      
+  foreach ($apiData['records'] as $record)
+  {
 
-$trash = "
-		UPDATE $wpdb->posts p
-        INNER JOIN $wpdb->postmeta pm ON (p.ID = pm.post_id AND pm.meta_key = '_aero_cpt') 
-		SET p.post_status = 'trash' 
-		WHERE pm.meta_value = '$parentId'"
-		;
-$results = $wpdb->get_results($trash);
-		
-		
-foreach ($apiData['records'] as $record)
-{
+  $record_id = $record['id'];
+  $record_name = $record['name']; 
+  $record_slug = $record['slug']; 
+  
+  $post_type = $parent->post_name;
+  
+  $dynamic = $parent->post_excerpt; //record_id or name
+  
+  if ($dynamic !== 'name')
+  {
+  $record_slug = $record_id;
+  }
+  
+  
+  $field_names = array_column($apiData['fields'], 'name'); // get just the types
 
-$record_id = $record['id'];
-$record_name = $record['name']; 
-$record_slug = $record['slug']; 
-$post_type = $parent->post_name;
-$field_names = array_column($apiData['fields'], 'name'); // get just the types
+  // find if theres a trashed post with this record id already
 
-// find if theres a trashed post with this record id already
+  $existing = get_posts(['post_type'=> $post_type,'post_status' => 'trash','numberposts' => 1,'meta_key' => '_aero_id', 'meta_value' => $record_id ]);
 
-$existing = get_posts(['post_type'=> $post_type,'post_status' => 'trash','numberposts' => 1,'meta_key' => '_aero_id', 'meta_value' => $record_id ]);
+  if ($existing){$existing_id = $existing[0]->id;}
 
-if ($existing){$existing_id = $existing[0]->id;}
+  $record_post = array(
+          'ID' => $existing_id,
+          'post_title' => $record_name,
+          'post_name' => $record_slug,
+		  'post_parent' => '',
+          'post_type' => $post_type,
+          'post_status' => 'publish'
+      );
+    
+  $record_post_id = wp_insert_post($record_post);
 
-$record_post = array(
-        'ID' => $existing_id,
-        'post_title' => $record_name,
-        'post_name' => $record_slug,
-		'post_parent' => '',
-        'post_type' => $post_type,
-        'post_status' => 'publish'
-    );
-	
-$record_post_id = wp_insert_post($record_post);
-
-update_post_meta ($record_post_id, '_aero_cpt', $parentId);
-update_post_meta ($record_post_id, '_aero_id', $record_id);
-
-
-if ($existing)
-{
-$response['message'] .= "<br>record $record_id : $record_name already exists as $record_post_id and is being updated.";
-}
-else
-{
-$response['message'] .= "<br>record $record_id : $record_name has been created as $record_post_id.";
-}
-
-
-
-foreach ($record['fields'] as $key=>$value)
-{
-
-$field_index = array_search($key, $field_names);
-
-$type = $apiData['fields'][$field_index]['type'];
+  update_post_meta ($record_post_id, '_aero_cpt', $parentId);
+  update_post_meta ($record_post_id, '_aero_id', $record_id);
 
 
-if ($type == 'attachment_img')
-{
-$value = $value[0]['thumbnails']['large']['url'];
-}
+  if ($existing)
+  {
+  $response['message'] .= "<br>record $record_id : $record_name already exists as $record_post_id and is being updated.";
+  }
+  else
+  {
+  $response['message'] .= "<br>record $record_id : $record_name has been created as $record_post_id.";
+  }
 
-if ($type == 'attachment_doc')
-{
-$value = $value[0]['url'];
-}
 
-update_post_meta ($record_post_id, "aero_$key", $value);
 
-$response['message'] .= "<br> ---> field $key of type $type has been added.";
+  foreach ($record['fields'] as $key=>$value)
+  {
 
-}
-// end foreach field
+  $field_index = array_search($key, $field_names);
+
+  $type = $apiData['fields'][$field_index]['type'];
+
+
+  if ($type == 'attachment_img')
+  {
+  $value = $value[0]['thumbnails']['large']['url'];
+  }
+
+  if ($type == 'attachment_doc')
+  {
+  $value = $value[0]['url'];
+  }
+
+  update_post_meta ($record_post_id, "aero_$key", $value);
+
+  $response['message'] .= "<br> ---> field $key of type $type has been added.";
+
+  }
+  // end foreach field
 
 
 }
 // end foreach record
 
+  }
+  else // some problem with api
+  {
+  $response['status'] = 'error';
+  update_post_meta ($parentId,'aero_sync_status','error');
+  $message = $apiData['status']['message'];
+  update_post_meta ($parentId,'aero_sync_message',$message);
+  $response['message'] = $message;
 }
-else // some problem with api
+
+//If doing AJAX
+
+if($isAjax)
 {
-$response['message'] .= "There was an error";
+die(json_encode($response));
 }
-
-
+else
+{
 return $response;
-
+}
 
 }
 // end function
@@ -311,7 +360,3 @@ function aeropageTokenApiCall($token)
     $downloads[$file_md5] = $download;
     $product->set_downloads( $downloads );
 	*/
-	
-	
-
-
